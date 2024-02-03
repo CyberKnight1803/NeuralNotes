9f0d32014daf082b7928c1d9092b620371ca171d
shvam0000 <shvam0000@gmail.com>
2024-02-03 15:40:37 -0500

    ⚰️ remove footer

--- neuralnotes-fe/components/shared/footer.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Footer = () => {
-  return <div className="h-32 bg-[#00AEA6]">Footer</div>;
-};
-
-export default Footer;
--- neuralnotes-fe/components/shared/layout.tsx
+++ neuralnotes-fe/components/shared/layout.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { LayoutProps } from '@/utils/props';
-import { Navbar, Footer } from './';
+import { Navbar } from './';
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="font-sans-serif">
       <Navbar />
       <div className="h-screen">{children}</div>
-      <Footer />
     </div>
   );
 };
